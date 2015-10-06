10989b39430a850ec274062f154f47cb91e67d2d
Huynh Le Ba Vinh <vinhhlb@asiantech.vn>
2015-10-06 09:54:40 +0700

    add sliding menu

---
+++ app/src/main/java/com/asiantech/membersmanager/fragment/DrawerFragment.java
@@ -0,0 +1,180 @@
+package com.asiantech.membersmanager.fragment;
+
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+
+import com.asiantech.membersmanager.R;
+import com.asiantech.membersmanager.adapter.NavigationDrawerAdapter;
+import com.asiantech.membersmanager.models.NavigationDrawerItem;
+
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.EFragment;
+import org.androidannotations.annotations.ViewById;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Copyright © 2015 AsianTech inc.
+ * Created by VinhHlb on 10/6/15.
+ */
+@EFragment(R.layout.fragment_drawer)
+public class DrawerFragment extends Fragment {
+    @ViewById(R.id.recycler)
+    RecyclerView mRecycleView;
+
+    private ActionBarDrawerToggle mDrawerToggle;
+    private DrawerLayout mDrawerLayout;
+    private NavigationDrawerAdapter mAdapter;
+    private View mContainView;
+    private static String[] mTitles = null;
+    private FragmentDrawerListener drawerListener;
+
+    @AfterViews
+    public void afterView() {
+        initView();
+        initData();
+        initListener();
+        setAdapter();
+    }
+
+    public void setDrawerListener(FragmentDrawerListener listener) {
+        this.drawerListener = listener;
+    }
+
+    private void initView() {
+        //DO nothing
+    }
+
+    private void initData() {
+        mTitles = getActivity().getResources().getStringArray(R.array.nav_drawer_labels);
+        mAdapter = new NavigationDrawerAdapter(getActivity(), getData());
+    }
+
+    private void initListener() {
+        mRecycleView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        mRecycleView.addOnItemTouchListener(new RecyclerTouchListener(getActivity(), mRecycleView, new ClickListener() {
+            @Override
+            public void onClick(View view, int position) {
+                drawerListener.onDrawerItemSelected(view, position);
+                mDrawerLayout.closeDrawer(mContainView);
+            }
+
+            @Override
+            public void onLongClick(View view, int position) {
+
+            }
+        }));
+    }
+
+    private void setAdapter() {
+        mAdapter = new NavigationDrawerAdapter(getActivity(), getData());
+        mRecycleView.setAdapter(mAdapter);
+    }
+
+    public static List<NavigationDrawerItem> getData() {
+        List<NavigationDrawerItem> data = new ArrayList<>();
+        // preparing navigation drawer items
+        for (int i = 0; i < mTitles.length; i++) {
+            NavigationDrawerItem navItem = new NavigationDrawerItem();
+            navItem.setTitle(mTitles[i]);
+            data.add(navItem);
+        }
+        return data;
+    }
+
+    public void setUp(int fragmentId, DrawerLayout drawerLayout, final Toolbar toolbar) {
+        mContainView = getActivity().findViewById(fragmentId);
+        mDrawerLayout = drawerLayout;
+        mDrawerToggle = new ActionBarDrawerToggle(getActivity(), drawerLayout, toolbar, R.string.drawer_open, R.string.drawer_close) {
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getActivity().invalidateOptionsMenu();
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                getActivity().invalidateOptionsMenu();
+            }
+
+            @Override
+            public void onDrawerSlide(View drawerView, float slideOffset) {
+                super.onDrawerSlide(drawerView, slideOffset);
+                toolbar.setAlpha(1 - slideOffset / 2);
+            }
+        };
+
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+        mDrawerLayout.post(new Runnable() {
+            @Override
+            public void run() {
+                mDrawerToggle.syncState();
+            }
+        });
+    }
+
+    static class RecyclerTouchListener implements RecyclerView.OnItemTouchListener {
+
+        private GestureDetector gestureDetector;
+        private ClickListener clickListener;
+
+        public RecyclerTouchListener(Context context, final RecyclerView recyclerView, final ClickListener clickListener) {
+            this.clickListener = clickListener;
+            gestureDetector = new GestureDetector(context, new GestureDetector.SimpleOnGestureListener() {
+                @Override
+                public boolean onSingleTapUp(MotionEvent e) {
+                    return true;
+                }
+
+                @Override
+                public void onLongPress(MotionEvent e) {
+                    View child = recyclerView.findChildViewUnder(e.getX(), e.getY());
+                    if (child != null && clickListener != null) {
+                        clickListener.onLongClick(child, recyclerView.getChildPosition(child));
+                    }
+                }
+            });
+        }
+
+        @Override
+        public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
+
+            View child = rv.findChildViewUnder(e.getX(), e.getY());
+            if (child != null && clickListener != null && gestureDetector.onTouchEvent(e)) {
+                clickListener.onClick(child, rv.getChildPosition(child));
+            }
+            return false;
+        }
+
+        @Override
+        public void onTouchEvent(RecyclerView rv, MotionEvent e) {
+
+        }
+
+        @Override
+        public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
+
+        }
+    }
+
+    public interface FragmentDrawerListener {
+        void onDrawerItemSelected(View view, int position);
+    }
+
+    public interface ClickListener {
+        void onClick(View view, int position);
+
+        void onLongClick(View view, int position);
+    }
+}
---
+++ app/src/main/java/com/asiantech/membersmanager/models/NavigationDrawerItem.java
@@ -0,0 +1,33 @@
+package com.asiantech.membersmanager.models;
+
+
+public class NavigationDrawerItem {
+    private boolean showNotify;
+    private String title;
+
+
+    public NavigationDrawerItem() {
+
+    }
+
+    public NavigationDrawerItem(boolean showNotify, String title) {
+        this.showNotify = showNotify;
+        this.title = title;
+    }
+
+    public boolean isShowNotify() {
+        return showNotify;
+    }
+
+    public void setShowNotify(boolean showNotify) {
+        this.showNotify = showNotify;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
