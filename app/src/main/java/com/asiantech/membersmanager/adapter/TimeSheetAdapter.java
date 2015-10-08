b39cd4d49d77feb8b19917e8c4ea82f01aa919eb
Huynh Le Ba Vinh <vinhhlb@asiantech.vn>
2015-10-08 10:35:27 +0700

    add recyclerview and handler chooseday,

---
+++ app/src/main/java/com/asiantech/membersmanager/adapter/TimeSheetAdapter.java
@@ -0,0 +1,106 @@
+package com.asiantech.membersmanager.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.asiantech.membersmanager.R;
+import com.asiantech.membersmanager.models.Notification;
+
+import java.util.ArrayList;
+
+/**
+ * Copyright © 2015 AsianTech inc.
+ * Created by VinhHlb on 10/8/15.
+ */
+public class TimeSheetAdapter extends RecyclerView.Adapter<TimeSheetAdapter.ViewHolder> {
+    private ArrayList<Notification> mDatas = new ArrayList<>();
+    private Context mContext;
+
+    public TimeSheetAdapter(Context context, ArrayList<Notification> datas) {
+        mContext = context;
+        mDatas = datas;
+    }
+
+    @Override
+    public TimeSheetAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View rootView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_recycle_home, parent, false);
+        ViewHolder viewHolder = new ViewHolder(rootView);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(TimeSheetAdapter.ViewHolder holder, final int position) {
+        holder.imgAvata.setImageResource(mDatas.get(position).getMAvata());
+        holder.tvSender.setText(mDatas.get(position).getMSender());
+        holder.tvTittle.setText(mDatas.get(position).getMTittle());
+        holder.tvContent.setText(mDatas.get(position).getMContent());
+        holder.tvTime.setText(mDatas.get(position).getMTime());
+
+        if (mDatas.get(position).getIsHot()) {
+            holder.imgHot.setVisibility(View.VISIBLE);
+        } else {
+            holder.imgHot.setVisibility(View.INVISIBLE);
+        }
+
+        checkFavorite(holder, position);
+        holder.rlTittle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                callDetail.OnCallDetails(mArraylists.get(position));
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDatas.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        com.asiantech.membersmanager.utils.CircleImageView imgAvata;
+        ImageView imgHot, imgFavorite, imgDelete;
+        TextView tvSender, tvTittle, tvContent, tvTime;
+        RelativeLayout rlTittle;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            imgAvata = (com.asiantech.membersmanager.utils.CircleImageView) itemView.findViewById(R.id.imgAvata);
+            tvSender = (TextView) itemView.findViewById(R.id.tvSender);
+            tvTittle = (TextView) itemView.findViewById(R.id.tvTittle);
+            tvContent = (TextView) itemView.findViewById(R.id.tvContent);
+            tvTime = (TextView) itemView.findViewById(R.id.tvTime);
+            imgHot = (ImageView) itemView.findViewById(R.id.imgHot);
+            imgFavorite = (ImageView) itemView.findViewById(R.id.imgFavorite);
+            imgDelete = (ImageView) itemView.findViewById(R.id.imgDelete);
+            rlTittle = (RelativeLayout) itemView.findViewById(R.id.rlTop);
+        }
+    }
+
+    private void checkFavorite(final ViewHolder holder, final int position) {
+        if (mDatas.get(position).getIsFavorite()) {
+            holder.imgFavorite.setImageResource(R.drawable.ic_favorite);
+        } else {
+            holder.imgFavorite.setImageResource(R.drawable.ic_unfavorite);
+        }
+
+        holder.imgFavorite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mDatas.get(position).getIsFavorite()) {
+                    holder.imgFavorite.setImageResource(R.drawable.ic_unfavorite);
+                    mDatas.get(position).setIsFavorite(false);
+                } else {
+                    holder.imgFavorite.setImageResource(R.drawable.ic_favorite);
+                    mDatas.get(position).setIsFavorite(true);
+                }
+            }
+        });
+    }
+}
--- app/src/main/java/com/asiantech/membersmanager/fragment/TimeSheetFragment.java
+++ app/src/main/java/com/asiantech/membersmanager/fragment/TimeSheetFragment.java
@@ -1,11 +1,20 @@
 package com.asiantech.membersmanager.fragment;
 
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
+
 import com.asiantech.membersmanager.R;
 import com.asiantech.membersmanager.abstracts.BaseFragment;
+import com.asiantech.membersmanager.adapter.TimeSheetAdapter;
+import com.asiantech.membersmanager.models.Notification;
+import com.asiantech.membersmanager.utils.MyLinearLayoutManager;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EFragment;
 import org.androidannotations.annotations.ViewById;
+
+import java.util.ArrayList;
 
 import me.nlmartian.silkcal.DatePickerController;
 import me.nlmartian.silkcal.DayPickerView;
@@ -17,12 +26,30 @@ import me.nlmartian.silkcal.SimpleMonthAdapter;
  */
 @EFragment(R.layout.fragment_timesheet)
 public class TimeSheetFragment extends BaseFragment implements DatePickerController {
+
+    private ArrayList<Notification> mArraylists = new ArrayList<>();
+    private TimeSheetAdapter mAdapter;
+
     @ViewById(R.id.calendar_view)
     DayPickerView mDayPickerView;
+    @ViewById(R.id.recycler_timeSheet)
+    RecyclerView mRecycleTimeSheet;
 
     @AfterViews
     public void afterView() {
+        initData();
+        setAdapter();
+        fakeData();
+    }
+
+    private void initData() {
+        mRecycleTimeSheet.setLayoutManager(new MyLinearLayoutManager(getActivity().getBaseContext(), LinearLayoutManager.VERTICAL, true));
         mDayPickerView.setController(this);
+    }
+
+    private void setAdapter() {
+        mAdapter = new TimeSheetAdapter(getActivity(), mArraylists);
+        mRecycleTimeSheet.setAdapter(mAdapter);
     }
 
     @Override
@@ -32,6 +59,123 @@ public class TimeSheetFragment extends BaseFragment implements DatePickerControl
             mOnBaseFragmentListener.setTitleHeader(getString(R.string.time_sheet));
     }
 
+    private void fakeData() {
+        Notification notification1 = new Notification();
+        notification1.setIsFavorite(true);
+        notification1.setIsHot(true);
+        notification1.setMAvata(R.drawable.p1);
+        notification1.setMContent("Thong basdsd fsdsdf");
+        notification1.setMSender("Le Thai Son");
+        notification1.setMTittle("Thong bao hop khan cap vinh");
+        notification1.setMTime("14:32 PM, 06/10");
+        mArraylists.add(notification1);
+
+        Notification notification2 = new Notification();
+        notification2.setIsFavorite(false);
+        notification2.setIsHot(false);
+        notification2.setMAvata(R.drawable.p2);
+        notification2.setMContent("Thong basdsd fsdsdf");
+        notification2.setMSender("Le Thai Son");
+        notification2.setMTittle("Thong bao hop khan cap");
+        notification2.setMTime("14:32 PM, 06/10");
+        mArraylists.add(notification2);
+
+        Notification notification3 = new Notification();
+        notification3.setIsFavorite(true);
+        notification3.setIsHot(false);
+        notification3.setMAvata(R.drawable.p3);
+        notification3.setMContent("Đã có lúc anh mong tim mình bé lại\n" +
+                "Để nỗi nhớ em không thể nào thêm nữa\n" +
+                "Đã có lúc anh mong ngừng thời gian trôi\n" +
+                "Để những dấu yêu sẽ không phai mờ\n" +
+                "\n" +
+                "Nếu không hát lên nặng lòng da diết\n" +
+                "Nếu không nói ra làm sao biết\n" +
+                "Anh thương em\n" +
+                "Anh sẽ nói em nghe những điều chưa bao giờ\n" +
+                "\n" +
+                "Bình minh khuất lấp sau màn đêm như nỗi lòng anh\n" +
+                "Chất chứa lâu nay em đâu nào hay biết\n" +
+                "Hoàng hôn tắt nắng hay vì anh không hiểu được em\n" +
+                "Dập tan bao yêu dấu lụi tàn\n" +
+                "\n" +
+                "Cất tiếng hát nghe sao lòng nhẹ cơn sầu\n" +
+                "Dẫu có chút vương, chút ân tình chôn giấu\n" +
+                "Đếm những nhớ thương thầm lặng trên tay\n" +
+                "Nghe sao buốt thêm, ướt đôi vai gầy.\n" +
+                "\n" +
+                "Nếu không hát lên nặng lòng da diết\n" +
+                "Nếu không nói ra làm sao biết\n" +
+                "Anh thương em\n" +
+                "Anh sẽ nói em nghe những điều chưa bao giờ\n" +
+                "\n" +
+                "Bình minh khuất lấp sau màn đêm như nỗi lòng anh\n" +
+                "Chất chứa lâu nay em đâu nào hay biết\n" +
+                "Hoàng hôn tắt nắng hay vì anh không hiểu được em\n" +
+                "Dập tan bao yêu dấu lụi tàn\n" +
+                "\n" +
+                "Vì anh câm nín chôn sâu yêu thương anh trao đến em,\n" +
+                "Lặng nhìn em lướt qua bên đời.\n" +
+                "Một mai ai biết cơn mê đưa em vào vòng tay mới.\n" +
+                "Anh sẽ chờ phía sau giấc mơ của em\n" +
+                "Anh sẽ chờ để nói những điều chưa bao giờ");
+        notification3.setMSender("Le Thai Son");
+        notification3.setMTittle("Thong bao hop khan cap");
+        notification3.setMTime("14:32 PM, 06/10");
+        mArraylists.add(notification3);
+
+        Notification notification4 = new Notification();
+        notification4.setIsFavorite(false);
+        notification4.setIsHot(false);
+        notification4.setMAvata(R.drawable.p4);
+        notification4.setMContent("Thong basdsd fsdsdfffgdfgdfgdf ");
+        notification4.setMSender("Le Thai Son");
+        notification4.setMTittle("Thong bao hop khan cap");
+        notification4.setMTime("14:32 PM, 06/10");
+        mArraylists.add(notification4);
+
+        Notification notification5 = new Notification();
+        notification5.setIsFavorite(true);
+        notification5.setIsHot(false);
+        notification5.setMAvata(R.drawable.p2);
+        notification5.setMContent("Thong basdsd ");
+        notification5.setMSender("Le Thai Son");
+        notification5.setMTittle("Thong bao hop khan cap");
+        notification5.setMTime("14:32 PM, 06/10");
+        mArraylists.add(notification5);
+
+        Notification notification6 = new Notification();
+        notification6.setIsFavorite(true);
+        notification6.setIsHot(false);
+        notification6.setMAvata(R.drawable.p3);
+        notification6.setMContent("Thong basdsd fsdsdfffgdfgdfgdf");
+        notification6.setMSender("Le Thai Son");
+        notification6.setMTittle("Thong bao hop khan cap");
+        notification6.setMTime("14:32 PM, 06/10");
+        mArraylists.add(notification6);
+
+        Notification notification7 = new Notification();
+        notification7.setIsFavorite(true);
+        notification7.setIsHot(false);
+        notification7.setMAvata(R.drawable.p1);
+        notification7.setMContent("Thong basdsd fsdsdfffgdfgdfgdf");
+        notification7.setMSender("Le Thai Son");
+        notification7.setMTittle("Thong bao hop khan cap");
+        notification7.setMTime("14:32 PM, 06/10");
+        mArraylists.add(notification7);
+
+        Notification notification8 = new Notification();
+        notification8.setIsFavorite(true);
+        notification8.setIsHot(false);
+        notification8.setMAvata(R.drawable.p1);
+        notification8.setMContent("Thong basdsd fsdsdfffgdfgdfgdf");
+        notification8.setMSender("Le Thai Son");
+        notification8.setMTittle("Thong bao hop khan cap");
+        notification8.setMTime("14:32 PM, 06/10");
+        mArraylists.add(notification8);
+        mAdapter.notifyDataSetChanged();
+    }
+
     @Override
     public int getMaxYear() {
         return 0;
@@ -39,7 +183,7 @@ public class TimeSheetFragment extends BaseFragment implements DatePickerControl
 
     @Override
     public void onDayOfMonthSelected(int year, int month, int day) {
-
+        Toast.makeText(getActivity(), "Reaquest API get notification on " + day + " - " + month + " - " + year, Toast.LENGTH_LONG).show();
     }
 
     @Override
