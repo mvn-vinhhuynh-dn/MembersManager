21e555822f6c1ec76d15af33e602a03b7ab8b2cc
Huynh Le Ba Vinh <vinhhlb@asiantech.vn>
2015-10-08 09:24:41 +0700

    add calendar view

--- app/src/main/java/com/asiantech/membersmanager/fragment/TimeSheetFragment.java
+++ app/src/main/java/com/asiantech/membersmanager/fragment/TimeSheetFragment.java
@@ -3,18 +3,47 @@ package com.asiantech.membersmanager.fragment;
 import com.asiantech.membersmanager.R;
 import com.asiantech.membersmanager.abstracts.BaseFragment;
 
+import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EFragment;
+import org.androidannotations.annotations.ViewById;
+
+import me.nlmartian.silkcal.DatePickerController;
+import me.nlmartian.silkcal.DayPickerView;
+import me.nlmartian.silkcal.SimpleMonthAdapter;
 
 /**
  * Copyright © 2015 AsianTech inc.
  * Created by VinhHlb on 10/6/15.
  */
 @EFragment(R.layout.fragment_timesheet)
-public class TimeSheetFragment extends BaseFragment {
+public class TimeSheetFragment extends BaseFragment implements DatePickerController {
+    @ViewById(R.id.calendar_view)
+    DayPickerView mDayPickerView;
+
+    @AfterViews
+    public void afterView() {
+        mDayPickerView.setController(this);
+    }
+
     @Override
     public void onResume() {
         super.onResume();
         if (mOnBaseFragmentListener != null)
             mOnBaseFragmentListener.setTitleHeader(getString(R.string.time_sheet));
+    }
+
+    @Override
+    public int getMaxYear() {
+        return 0;
+    }
+
+    @Override
+    public void onDayOfMonthSelected(int year, int month, int day) {
+
+    }
+
+    @Override
+    public void onDateRangeSelected(SimpleMonthAdapter.SelectedDays<SimpleMonthAdapter.CalendarDay> selectedDays) {
+
     }
 }
